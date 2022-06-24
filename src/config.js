d9b93bd3c86360987694d739173b9602db091800
Alfredo Blanco <alfre.blanco@icloud.com>
2022-06-24 19:53:12 -0300

    Finished

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ module.exports = {
     DB_USER : process.env.DB_USER || 'postgres',
     DB_PASSWORD : process.env.DB_PASSWORD || 'Alfredo',
     DB_HOST : process.env.DB_HOST || 'localhost:5432',
-    SENDGRID_API_KEY : process.env.SENDGRID_API_KEY || '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',
-    SENDER : process.env.SENDER || 'alfre.blanco@icloud.com'
+    SENDGRID_API_KEY : process.env.SENDGRID_API_KEY || 'Your Sendgrip Api Key',
+    SENDER : process.env.SENDER || 'Your sender address'
 }
